685b3a46fa182abe21ed2c4a8855dd2026933f2f
seisdoug <douglashelen@comcast.net>
2015-07-22 14:53:46 -0400

    Initial commit of Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions  aCacheMatrix and cacheSolve use scoping rules to store
+## and retrieve a special matrix and compute, store, and retrieve its invse
 
-## Write a short comment describing this function
+## The function makeCacheMatrix defines and stores two functions, set to 
+## store the matrix, and get to retrieve the matrix. The matrix is cached
+## in the parent or global environment. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mat <- NULL
+        set <- function(y) {
+                x <<- y
+                mat <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inverse) mat <<- inverse
+        getinverse <- function() mat
+        list(set = set, get = get,
+             setinverse=setinverse,
+             getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve retrieves the inverse of a special matrix,
+## if it has been computed, or computes the inverse using the function
+## solve it has not been previously computed. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mat <- x$getinverse()
+        if(!is.null(mat)) {
+                message("getting cached data")
+                return(mat)
+        }
+        data <- x$get()
+        mat <- solve(data)
+        x$setinverse(mat)
+        mat
 }
